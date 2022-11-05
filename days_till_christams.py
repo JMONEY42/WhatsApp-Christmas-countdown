549a8f17cdbcafca1d3f994c3518196bca666189
Jonathan4296 <117517659+JMONEY42@users.noreply.github.com>
2022-11-05 12:00:27 +0000

    Removing Unnecessary Code

--- days_till_christams.py
+++ days_till_christams.py
@@ -2,8 +2,7 @@ import pywhatkit as pwk
 from datetime import date 
 import random, os
 
-GROUP_LINK = "ENTER_GROOP_LINK"
-PHONE_NUMBER = "ENTER_PHONE_NUMBER"
+MESSAGE_LINK = "ENTER_MESSAGE_LINK"
 IMAGE_PATH = "PATH_FOR_IMAGES"
 
 def random_num():
@@ -13,9 +12,8 @@ def random_num():
 def send_message():
     daysTillChristmas = days_till_christmas()
     image = random_num()
-    #pwk.sendwhatmsg_to_group_instantly(PHONE_NUMBER, f"Only {daysTillChristmas} days till christmas", 10, tab_close=True) # send message to phone number 
     pwk.sendwhats_image\
-        (GROUP_LINK, f"{IMAGE_PATH}{image}",\
+        (MESSAGE_LINK, f"{IMAGE_PATH}{image}",\
              f"\U0001F973 \U0001F384 \U00002744 Only {daysTillChristmas} days till Christmas \U0001F973 \U0001F384 \U00002744", tab_close=True) # send message to grope
 
 def days_till_christmas():
